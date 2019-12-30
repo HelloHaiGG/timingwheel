f06b765c1e9a7071b9146711925eb2ae66032198
xqh <1296985852@qq.com>
2019-12-30 05:30:53 -0500

    bigcache

--- common/icache/L1/l1.go
+++ common/icache/L1/l1.go
@@ -1,27 +1,29 @@
 package L1
 
 import (
+	"errors"
 	"github.com/allegro/bigcache"
 	"log"
+	"sync"
 	"time"
 )
 
 /**
 big-cache
+一级缓存存储 热点数据 基本不会改变的数据
 */
 
 var bCache *bigcache.BigCache
+var mFlag sync.Map
 
 func init() {
 	cfg := bigcache.Config{
 		Shards:             2 << 10, //缓存分片数
-		LifeWindow:         time.Second * 60,
-		CleanWindow:        3,
-		MaxEntriesInWindow: 2 << 10,
+		LifeWindow:         time.Second * 10,
+		CleanWindow:        2,
+		MaxEntriesInWindow: 2 << 10, //2M
 		MaxEntrySize:       2,
-		OnRemove:           nil, //删除回调
-		OnRemoveWithReason: nil,
-		Logger:             nil, //logger
+		OnRemove:           onDelete, //删除回调
 	}
 	if c, err := bigcache.NewBigCache(cfg); err != nil {
 		log.Fatal("Init big-cache error:", err)
@@ -30,16 +32,48 @@ func init() {
 	}
 }
 
-func GetL1(key string) (string, error) {
-	bCache.Get(key)
-	return "", nil
+func Get(key string) (string, error) {
+	var retry int
+RETRY:
+	if retry < 3 {
+		if v, ok := mFlag.Load(key); ok && v.(bool) {
+			retry++
+			time.Sleep(time.Millisecond * 50 * 1 << retry) //100,200,400
+			goto RETRY
+		}
+	} else {
+		return "", errors.New("Key has be used. ")
+	}
+	b, err := bCache.Get(key)
+	return string(b), err
 }
 
-func PutL1(key, value string) error {
-	bCache.Set(key, []byte(value))
+func Put(key, value string) error {
+	return Reset(key, value)
+}
+
+func Reset(key, value string) error {
+	var retry int
+RETRY:
+	if retry < 3 {
+		if v, ok := mFlag.Load(key); ok && v.(bool) {
+			retry++
+			time.Sleep(time.Millisecond * 50 * 1 << retry) //100,200,400
+			goto RETRY
+		}
+	} else {
+		return errors.New("Key has be used. ")
+	}
+	mFlag.Store(key, true)
+	_ = bCache.Delete(key)
+	if err := bCache.Set(key, []byte(value)); err != nil {
+		return err
+	}
+	mFlag.Delete(key)
 	return nil
 }
 
-func ResetL1(key, value string) error {
-	return nil
+//删除缓存回调
+func onDelete(key string, entry []byte) {
+
 }
