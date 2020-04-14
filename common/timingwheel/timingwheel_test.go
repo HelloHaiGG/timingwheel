39430715270e584354533d82553876d1c8485144
mac126 <mac126@mac126deMacBook-Pro.local>
2020-04-14 09:45:36 +0800

    test: 多任务测试

--- common/timingwheel/timingwheel_test.go
+++ common/timingwheel/timingwheel_test.go
@@ -7,38 +7,41 @@ import (
 )
 
 type HTask struct {
-
 }
 
-func (p *HTask)Init()  {
+func (p *HTask) Init() {
 	fmt.Println("H Task Init...")
 }
-func (p *HTask)Perform() error {
+func (p *HTask) Perform() error {
 	fmt.Println("H Task Perform...")
 	return nil
 }
-func (p *HTask)OnStop() error {
+func (p *HTask) OnStop() error {
 	fmt.Println("H Task Stop...")
 	return nil
 }
 
 func TestTimingWheel(t *testing.T) {
-	tw := NewTimingWheel(time.Second,3)
+
+	tw := NewTimingWheel(time.Second, 3)
 	tw.Start()
-	id,err := tw.AddTask(&HTask{},&Options{
-		TimingTime: 10,
-		IsRepeat:   true,
-	})
-	go func() {
-		time.Sleep(time.Second * 11)
-		if tw.DelTask(id){
-			fmt.Println("del success.")
-		}else{
-			fmt.Println("sss")
-		}
-	}()
-	fmt.Println(id,err)
-	for{
+	for i := 0; i < 100; i++ {
+		id, err := tw.AddTask(&HTask{}, &Options{
+			TimingTime: 1,
+			IsRepeat:   false,
+		})
+		fmt.Println(id, err)
+	}
+	//go func() {
+	//	time.Sleep(time.Second * 11)
+	//	if tw.DelTask(id) {
+	//		fmt.Println("del success.")
+	//	} else {
+	//		fmt.Println("sss")
+	//	}
+	//}()
+
+	for {
 		t := time.NewTicker(time.Second)
 		select {
 		case <-t.C:
